ef071554616cf8af9919667cb4af733dade2a28b
cmohri <clara.mohri@gmail.com>
2019-04-30 19:23:19 -0400

    Add accessor

--- 22/closure.py
+++ 22/closure.py
@@ -1,3 +1,10 @@
+'''
+Clara Mohri
+SoftDev2 Pd06
+K22 -- Closure
+2019-05-01
+'''
+
 def repeat(string):
     return lambda x: x * string
 
@@ -7,18 +14,7 @@ print(r1(2))
 r2 = repeat("goodbye")
 print(r2(2))
 
-"""
-def outer():
-    x = "foo"
-    def inner():
-        #nonlocal x
-        x = "bat"
-    inner()
-    return x
-
-print(outer())
-"""
-
+'''
 def outer():
     x =	"foo"
     def	inner():
@@ -29,12 +25,24 @@ def outer():
 
 print(outer())
 
-
+'''
 def make_counter():
     x = 0
     def inc():
         nonlocal x
         x += 1
         return x
-    return inc
+    def access():
+        nonlocal x
+        return x
+    return( inc, access)
     
+ctr1, acc1 = make_counter()
+
+print(ctr1())
+print(acc1())
+print(ctr1())
+print(ctr1())
+print(ctr1())
+print(ctr1())
+print(acc1())
